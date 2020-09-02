2fc4f649e660d7c5eadae851fa3f1b96c6b537f8
SurdTechnologies <57554132+SurdTechnologies@users.noreply.github.com>
2020-09-02 17:36:50 +0530

    Fix codebuild error

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
     # s3 bucket region
     encrypt = true
     # encrypt bucket contents
-    bucket = "${BUCKET_NAME}"
+    bucket = "surd-demo-terraform-dev"
     # s3 bucket name
     key = "terraform.tfstate"
     # terraform state file name
-    kms_key_id = "${KMS_KEY_ID}"
+    kms_key_id = "arn:aws:kms:us-east-1:033124433982:key/dedef1e5-9d61-40d7-a44f-6f1735591057"
     # optional
     dynamodb_table = "terraform-lock"
     # dynamoDB table name
